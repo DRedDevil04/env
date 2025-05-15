a3b9c8214e6a196fb221d62b8a8228c976b4971a
DRedDevil04 <devamydesai@gmail.com>
2025-05-15 16:05:51 +0530

    [init] : add zsh and nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("devam.plugins-setup")
+require("devam.core.options")
+require("devam.core.colorscheme")
+require("devam.core.keymaps")
---
+++ .config/nvim/lua/devam/core/keymaps.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- comsiseness
+
+-- general keymaps
+
+keymap.set("i","jk", "<ESC>")
+keymap.set("n","<leader>nh","nohl<CR>")
+
